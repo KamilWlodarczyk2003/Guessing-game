4ca96fb017e0f7b2a9723211a877a1e8b2f3bf18
Kamil Wlodarczyk <KamilWlodarczyk2003@gmail.com>
2023-05-02 01:37:49 +0200

    Going to sleep rn, commiting all I did

--- Guessing.py
+++ Guessing.py
@@ -1,3 +1,23 @@
 from random import randint
 
 random_pick=randint(0,100)
+level = input('Pick "Hard" or "Easy" level' ).lower()
+
+def Guessing(random_pick, pick):
+    if pick == random_pick:
+        return "You guessed correctly"
+    elif pick > random_pick:
+        return "Too high"
+    else:
+        return "Too low"
+
+if level=="hard":
+    attempts=5
+    for x in range(5):
+        print(f"You have {attempts} attempts")
+        pick = input("Pick number between 1 and 100 \n")
+        print(Guessing(random_pick))
+
+else:
+    print("Wrong input")
+
