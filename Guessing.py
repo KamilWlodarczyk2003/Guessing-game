4098c24e59bb473afc1153633239ee3a6dc19d09
Kamil Wlodarczyk <KamilWlodarczyk2003@gmail.com>
2023-05-02 20:21:01 +0200

    Finished it

--- Guessing.py
+++ Guessing.py
@@ -15,9 +15,20 @@ if level=="hard":
     attempts=5
     for x in range(5):
         print(f"You have {attempts} attempts")
-        pick = input("Pick number between 1 and 100 \n")
-        print(Guessing(random_pick))
+        pick = int(input("Pick number between 1 and 100 \n"))
+        print(Guessing(random_pick, pick))
+        if Guessing(random_pick, pick)=="You guessed correctly":
+            break
+        attempts-=1
+elif level=="easy":
+    attempts=10
+    for x in range(10):
+        print(f"You have {attempts} attempts")
+        pick = int(input("Pick number between 1 and 100 \n"))
+        print(Guessing(random_pick, pick))
+        if Guessing(random_pick, pick)=="You guessed correctly":
+            break
+        attempts-=1
 
 else:
     print("Wrong input")
-
